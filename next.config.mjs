4c6e313c434368e6709ee13f23ac7960316b3752
mbina <mbn.mdn.82@gmail.com>
2024-10-18 17:19:24 +0330

    Updating Token

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        AUTH_TOKEN: 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MjkyNTkxNzgsInN1YiI6ImE2MmM5MmRiLTQ5MTktNGU4MS04ZjZjLWE5ODEzNTkxMzlhYSJ9.iKj3Rew29rRuBqrYmV3ltUok3DffU0zL-M3Vg2wbuyNefGXu1IUAaP9ZV1wfwkvaBBAycU8CaA-6-yUNjBon7g',
+        AUTH_TOKEN: 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MjkyNjY1MTgsInN1YiI6ImE2MmM5MmRiLTQ5MTktNGU4MS04ZjZjLWE5ODEzNTkxMzlhYSJ9.ph0LYZCf2u_zHcKo6uWdK8y4q3gtuywBfms4yfhuso5fbfOi6wCIIo5QzY4Tfo2XHig_9ymBHp4hpMw8RGtnhQ',
     },
 };
 
